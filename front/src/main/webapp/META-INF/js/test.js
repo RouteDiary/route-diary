68810d92a1756ac860881471c4c7769afbcc51dd
BACKMINSEONG <msk7006@naver.com>
2022-07-04 12:21:33 +0900

    Chore: settings.json 파일 수정

---
+++ front/src/main/webapp/META-INF/js/test.js
@@ -0,0 +1,14 @@
+var a;
+console.log(typeof a);
+a = 123;
+console.log(typeof a);
+a = "hellso";
+console.log(typeof a);
+a = true;
+
+console.log(typeof a);
+
+a = function () {};
+console.log(typeof a);
+a = {};
+console.log(typeof a);
