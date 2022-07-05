67cd7d8116d88a4a296677ddbb885ed8ea5d2419
koreayong19 <56390797+koreayong19@users.noreply.github.com>
2022-07-05 16:16:21 +0900

    Fix : 버그 수정
    트리거 diary.no 번호마다 안오르던거 수정

--- db/index/trigger.sql
+++ db/index/trigger.sql
@@ -1,10 +1,10 @@
-create trigger like_cnt_trig
+create or replace trigger like_cnt_trig
 after insert or delete on likes
 for each row
 begin
 if inserting then
-    update diaries set diary_like_cnt = (nvl(diary_like_cnt,0) + 1);
+    update diaries set diary_like_cnt = (nvl(diary_like_cnt,0) + 1) where :new.diary_no = diaries.diary_no;
 elsif deleting then 
-    update diaries set diary_like_cnt = (nvl(diary_like_cnt,0) - 1);
+    update diaries set diary_like_cnt = (nvl(diary_like_cnt,0) - 1) where :old.diary_no = diaries.diary_no;
 end if;
 end;
