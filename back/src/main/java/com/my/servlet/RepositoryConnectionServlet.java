1a7ca25ee3f3f3faecce744a47e24b314367555a
BACKMINSEONG <msk7006@naver.com>
2022-07-08 12:41:45 +0900

    ClientOracleRepository 완성
    REF: #12

--- back/src/main/java/com/my/servlet/RepositoryConnectionServlet.java
+++ back/src/main/java/com/my/servlet/RepositoryConnectionServlet.java
@@ -2,16 +2,15 @@ package com.my.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.my.dto.Diary;
-import com.my.exception.SelectException;
-import com.my.repository.DiaryOracleRepository;
-import com.my.repository.DiaryRepository;
+import com.my.dto.Client;
+import com.my.exception.UpdateException;
+import com.my.repository.ClientOracleRepository;
+import com.my.repository.ClientRepository;
 
 /**
  * Servlet implementation class RepositoryConnectionServlet
@@ -25,39 +24,18 @@ public class RepositoryConnectionServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     String envPath = getServletContext().getRealPath("project.properties");
-    DiaryRepository diaryRepository = new DiaryOracleRepository(envPath);
+    ClientRepository clientRepository = new ClientOracleRepository(envPath);
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
 
-    // 테스트용 코드
+    Client client = new Client("a2", "b2", "c2", "d2", 0);
     try {
-      List<Diary> diaries = diaryRepository.selectDirariesByWritingDate(11, 20);
-      List<Diary> diaries2 = diaryRepository.selectDirariesByViewCnt(11, 20);
-      List<Diary> diaries3 = diaryRepository.selectDirariesByLikeCnt(11, 20);
-      List<Diary> diaries4 = diaryRepository.selectDirariesById("a", 11, 20);
-      List<Diary> diaries5 = diaryRepository.selectDirariesByKeyword("경복궁", 11, 20);
-      for (int i = 0; i < diaries.size(); i++) {
-        out.print(i + 1 + " : " + diaries.get(i).toString() + "<br>");
-      }
-      out.print("<br> -------------<br>");
-      for (int i = 0; i < diaries2.size(); i++) {
-        out.print(i + 1 + " : " + diaries2.get(i).toString() + "<br>");
-      }
-      out.print("<br> -------------<br>");
-      for (int i = 0; i < diaries3.size(); i++) {
-        out.print(i + 1 + " : " + diaries3.get(i).toString() + "<br>");
-      }
-      out.print("<br> -------------<br>");
-      for (int i = 0; i < diaries4.size(); i++) {
-        out.print(i + 1 + " : " + diaries4.get(i).toString() + "<br>");
-      }
-      out.print("<br> -------------<br>");
-      for (int i = 0; i < diaries5.size(); i++) {
-        out.print(i + 1 + " : " + diaries5.get(i).toString() + "<br>");
-      }
-    } catch (SelectException e) {
+      clientRepository.update(client);
+    } catch (UpdateException e) {
+      // TODO Auto-generated catch block
       e.printStackTrace();
     }
+    out.print(client.toString() + "<br>");
 
   }
 
