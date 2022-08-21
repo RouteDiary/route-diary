2b19781e6c33ddac2a61c8439281ad93c76ed2ed
kwonminho <kwonmh1992@gmail.com>
2022-08-21 20:55:38 +0900

    Feat: back을 Spring Boot로 변경 (현재 껍데기만 있음)

--- back/src/main/java/com/my/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.my.repository;
-
-import com.my.dto.Admin;
-import com.my.exception.SelectException;
-
-public interface AdminRepository {
-  Admin selectAdminByIdAndPwd(String adminId, String adminPwd) throws SelectException;
-
-}
