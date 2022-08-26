f687571b070ced0d9357bcb3988380cc6a8bc978
kmh <69135840+kwonminho1992@users.noreply.github.com>
2022-08-26 10:20:59 +0900

    Feat: MyBatis 설정 완료
    mapper, application.properties, testcode 등을 만듦.
    test를 바탕으로 각자 repository 코드를 작성하시면 됩니다.
    
    Co-Authored-By: BACKMINSEONG <99784071+BACKMINSEONG@users.noreply.github.com>

--- back/src/main/java/com/routediary/dto/Diary.java
+++ back/src/main/java/com/routediary/dto/Diary.java
@@ -1,6 +1,7 @@
 package com.routediary.dto;
 
-import java.sql.Date;
+
+import java.util.Date;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
@@ -13,24 +14,23 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Diary {
   @NonNull
   private int diaryNo;
   private String diaryTitle;
   @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss", timezone = "Asia/Seoul")
   private Date diaryWritingTime;
+  @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss", timezone = "Asia/Seoul")
+  private Date diaryModifyingTime;
+  @JsonFormat(pattern = "yyyy/MM/dd", timezone = "Asia/Seoul")
   private Date diaryStartDate;
+  @JsonFormat(pattern = "yyyy/MM/dd", timezone = "Asia/Seoul")
   private Date diaryEndDate;
-  private int diaryDisclosureFlag;
+  private Integer diaryDisclosureFlag;
   private int diaryViewCnt;
   private int diaryLikeCnt;
-  @NonNull
   private Client client;
-  @NonNull
   private List<Route> routes;
   private List<Comment> comments;
-  private List<Comment> hashtags;
-  private Like like;
-  private Date diaryModifyingTime;
+  private List<Hashtag> hashtags;
 }
--- back/src/main/java/com/routediary/dto/Notice.java
+++ back/src/main/java/com/routediary/dto/Notice.java
@@ -5,16 +5,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Notice {
-  @NonNull
   private int noticeNo;
   private String noticeTitle;
   private String noticeContent;
@@ -22,8 +19,7 @@ public class Notice {
   private Date noticeWritingTime;
   @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss", timezone = "Asia/Seoul")
   private Date noticeModifyingTime;
-  private int noticeViewCnt;
-  @NonNull
+  private Integer noticeViewCnt;
   private String adminId;
 
 }
