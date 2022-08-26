5df53b305bbbec9e91ba37ca392278e3b4a370f2
KMJbella <kmj960803@naver.com>
2022-08-26 12:21:49 +0900

    clientRepo 생성

---
+++ back/src/main/java/com/routediary/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.routediary.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface ClientRepository {
+	
+}
