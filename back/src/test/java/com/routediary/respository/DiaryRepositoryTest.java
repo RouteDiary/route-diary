9655bb567c04506fd0789056227acaf7d23fac20
kmh <69135840+kwonminho1992@users.noreply.github.com>
2022-08-26 17:03:16 +0900

    Feat: diaryRepository selectDiary() 만드는중
    만든 메서드
    selectCount()
    selectDiariesByClientId()
    selectDiary()
    
    만드는중
    selectDiaries()
    
    Ref: #12

---
+++ back/src/test/java/com/routediary/respository/DiaryRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.routediary.respository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.routediary.dto.Diary;
+import com.routediary.exception.SelectException;
+import com.routediary.repository.DiaryRepository;
+
+@SpringBootTest
+class DiaryRepositoryTest {
+  @Autowired
+  DiaryRepository diaryRepository;
+
+  @Test
+  void selectCountTest() throws SelectException {
+    int expectedCount = 9;
+    int count = diaryRepository.selectCount();
+    assertEquals(expectedCount, count);
+  }
+
+  @Test
+  void selectDiariesByClientIdTest() throws SelectException {
+    String clientId = "koreaman@gmail.com";
+    String expectedTitle = "즐거운 서울로 떠나요2";
+    String expectedClientNickname = "한쿡";
+    int expectedDiariesCount = 2;
+    List<Diary> diaries = diaryRepository.selectDiariesByClientId(clientId, 1, 10);
+    assertEquals(expectedTitle, diaries.get(0).getDiaryTitle());
+    assertEquals(expectedClientNickname, diaries.get(0).getClient().getClientNickname());
+    assertEquals(expectedDiariesCount, diaries.size());
+    System.out.println(diaries.toString());
+  }
+
+  @Test
+  void selectDiariesTest() throws SelectException {
+    int order = 1; // diary_view_cnt
+    String expectedTitle = "즐거운 광주으로 떠나요";
+    String expectedClientNickname = "한국남자4";
+    int expectedDiariesCount = 6;
+    List<Diary> diaries = diaryRepository.selectDiaries(order, 1, 10);
+    assertEquals(expectedTitle, diaries.get(0).getDiaryTitle());
+    assertEquals(expectedClientNickname, diaries.get(0).getClient().getClientNickname());
+    assertEquals(expectedDiariesCount, diaries.size());
+    System.out.println(diaries.toString());
+  }
+
+  @Test
+  void selectDiaryTest() throws SelectException {
+    int diaryNo = 1;
+    int expectedRoutesCount = 5;
+    String expectedTitle = "즐거운 서울로 떠나요";
+    String expectedClientNickname = "한쿡";
+    Diary diary = diaryRepository.selectDiary(diaryNo);
+    assertEquals(expectedTitle, diary.getDiaryTitle());
+    assertEquals(expectedClientNickname, diary.getClient().getClientNickname());
+    assertEquals(expectedRoutesCount, diary.getRoutes().size());
+    System.out.println(diary.toString());
+  }
+}
