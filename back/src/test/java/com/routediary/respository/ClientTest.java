abbd57647f3b489f30f5aaa2b2196168af2a0083
KMJbella <kmj960803@naver.com>
2022-08-28 02:13:37 +0900

    test 수정

--- back/src/test/java/com/routediary/respository/ClientTest.java
+++ back/src/test/java/com/routediary/respository/ClientTest.java
@@ -24,7 +24,7 @@ class ClientTest {
 
 	@Test
 	void SelectClientByIdTest() throws SelectException{
-		String clientId = "koreawoman@gmail.com";
+		String clientId = "kkk@gmail.com";
 		//회원 ID를 조회한다
 		Optional<Client> optId = Optional.ofNullable(repository.selectClientById(clientId));
 		assertTrue(optId.isPresent());
@@ -42,10 +42,12 @@ class ClientTest {
 	@Test
 	void InsertTest() throws InsertException{
 		Client client = new Client();
+		//회원가입시 들어갈 정보
 		client.setClientId("kmj@naver.com");
 		client.setClientPwd("kmj0803");
 		client.setClientCellphoneNo("010-1234-5797");
 		client.setClientNickname("만듀요");
+		client.setClientStatusFlag(1); //not null 컬럼이라 null값을 받아오지못함
 		repository.insert(client);
 
 	}
@@ -53,17 +55,21 @@ class ClientTest {
 	@Test
 	void UpdateTest() throws UpdateException, InsertException{
 		Client client1 = new Client();
-		client1.setClientPwd("kmj0803");
-		client1.setClientCellphoneNo("010-1234-5797");
-		client1.setClientNickname("만듀요");
-		client1.setClientStatusFlag(1);
-		repository.insert(client1);
+//		client1.setClientPwd("kmj0803");
+//		client1.setClientCellphoneNo("010-1234-5797");
+//		client1.setClientNickname("만듀일까");
+//		client1.setClientStatusFlag(0);
+//		repository.insert(client1);
 		
-		client1.setClientPwd("kmj0803");
+		// 업데이트될 정보
+		client1.setClientId("kmj@naver.com"); //not null 컬럼이라 null값을 받아오지못함
+		client1.setClientPwd("kmj0308");
 		client1.setClientCellphoneNo("010-1234-5797");
 		client1.setClientNickname("만듀임");
-		client1.setClientStatusFlag(1);
-		assertequals(client1.getClientNickname(), "만듀임");
+		client1.setClientStatusFlag(0);
+		repository.update(client1);
+		assertEquals(client1.getClientNickname(), "만듀임"); //왜 같은지 확인?
+		
 	}
 	
 }
