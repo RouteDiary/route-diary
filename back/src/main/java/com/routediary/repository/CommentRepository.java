b0838900473746ca9c6ea0cf79ab62e01dd6b6c2
lemonjunnn <meoal1206@gmail.com>
2022-08-28 11:12:02 +0900

    Feat: commentTest 완료

--- back/src/main/java/com/routediary/repository/CommentRepository.java
+++ back/src/main/java/com/routediary/repository/CommentRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package com.routediary.repository;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -28,50 +27,18 @@ public interface CommentRepository {
   public void update(Comment comment) throws UpdateException;
 
   /**
-   * 댓글을 삭제한다, 다이어리를 지웠을 때 댓글까지 전체 삭제한다
+   * 댓글을 삭제한다
    * 
    * @param comment
    * @throws DeleteException
    */
-  public void delete(Comment comment) throws DeleteException;
-}
-=======
-package com.routediary.repository;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-import com.routediary.dto.Client;
-import com.routediary.dto.Comment;
-import com.routediary.exception.DeleteException;
-import com.routediary.exception.InsertException;
-import com.routediary.exception.UpdateException;
-
-@Repository
-@Mapper
-public interface CommentRepository {
-  /**
-   * 댓글을 작성한다
-   * 
-   * @param comment
-   * @throws InsertException
-   */
-  void insert(Comment comment) throws InsertException;
+  public void delete(int diaryNo, int commentNo) throws DeleteException;
 
   /**
-   * 댓글을 수정한다
+   * 다이어리 diaryNo의 모든 행을 삭제한다
    * 
-   * @param comment
-   * @throws UpdateException
-   */
-  public void update(Comment comment) throws UpdateException;
-
-  /**
-   * 댓글을 삭제한다, 다이어리를 지웠을 때 댓글까지 전체 삭제한다
-   * 
-   * @param comment
+   * @param diaryNo
    * @throws DeleteException
    */
-  public void delete(Comment comment) throws DeleteException;
+  public void deleteAll(int diaryNo) throws DeleteException;
 }
->>>>>>> Stashed changes
