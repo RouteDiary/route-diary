83b7e9985ba7048096a276103f7242b6918036fd
BACKMINSEONG <msk7006@naver.com>
2022-08-28 11:14:28 +0900

    feat: notice 수정중
    .

--- back/src/test/java/com/routediary/respository/NoticeTest.java
+++ back/src/test/java/com/routediary/respository/NoticeTest.java
@@ -27,22 +27,22 @@ class NoticeTest {
 		notice.setNoticeContent("만두를좋아하는사람");
 		repository.insert(notice);
 	}
-	
+
 	@Test
 	void UpdateTest() throws UpdateException, InsertException{
-		
+
 		Notice notice1 = new Notice();
 		notice1.setAdminId("msk");
 		notice1.setNoticeTitle("sorry");
 		notice1.setNoticeContent("만두를좋아하는사람");
 		repository.insert(notice1);
-		
+
 //		notice1.setNoticeTitle("강만두");
 //		notice1.setNoticeNo(notice1.getNoticeNo());
 		notice1.setNoticeContent("만두를안좋아함");
 		repository.update(notice1);
 		assertEquals(notice1.getNoticeContent(), "만두를안좋아함");
-		
+
 	}
 
 }
