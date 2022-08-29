9af59b219b2656384962bd2322925de15966b12f
BACKMINSEONG <msk7006@naver.com>
2022-08-29 14:22:42 +0900

    Chore: .gitignore 수정

---
+++ back/src/main/java/com/routediary/dto/HashTag.java
@@ -0,0 +1,15 @@
+package com.routediary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hashtag {
+  private int diaryNo;
+  private String hashtag;
+}
