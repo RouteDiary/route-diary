2ae828b22418b65669441b0032233dcfd0a5b331
Bella <96408941+KMJbella@users.noreply.github.com>
2022-08-29 15:04:09 +0900

    fix: insert 수정
    완료

--- back/src/test/java/com/routediary/respository/RouteRepositoryTest.java
+++ back/src/test/java/com/routediary/respository/RouteRepositoryTest.java
@@ -16,42 +16,40 @@ class RouteRepositoryTest {
   @Autowired
   RouteRepository routeRepository;
 
-  // @Test
+   @Test
   void selectCountTest() throws SelectException {
-    int diaryNo = 1;
+    int diaryNo = 2;
     int count = routeRepository.selectCount(diaryNo);
     int expectedCount = 5;
     assertEquals(expectedCount, count);
   }
 
-  // @Test
+   @Test
   void insertTest() throws InsertException {
-    int diaryNo = 2;
-    int routeNo = 7;
-    String routeCotent = "ILikeTest";
-    String kakaoMapId = "1";
+    int diaryNo = 3;
+    String routeCotent = "ILikeParty";
+    String kakaoMapId = "4";
     Route route = new Route(); // route 객체 생성
     route.setDiaryNo(diaryNo); // route 객체의 diaryNo 멤버변수의 값을 설정
-    route.setRouteNo(routeNo);
     route.setRouteContent(routeCotent);
     route.setKakaoMapId(kakaoMapId);
 
     routeRepository.insert(route);
   }
 
-  // @Test
+   @Test
   void deleteTest() throws DeleteException {
     int diaryNo = 3;
-    int routeNo = 11;
+    int routeNo = 3;
 
     routeRepository.delete(diaryNo, routeNo);
   }
 
   @Test
   void deleteAllTest() throws DeleteException {
-    int diaryNo = 2;
+    int diaryNo = 3;
 
     routeRepository.deleteAll(diaryNo);
   }
-
 }
+
